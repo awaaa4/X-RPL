952b989036e8723bdb96aff25c0f2cd9c053cbc4
Tsabitah Asilah Najwa <asilahnajwa04@gmail.com>
2025-10-26 22:27:46 +0700

    Add files via upload

---
+++ Kelas/23-OKTOBER-2025/register.php
@@ -0,0 +1,8 @@
+<h1>register</h1>
+
+<form action="">
+    <input type="text" name="nama" placeholder="Nama Lengkap"> <br>
+    <input type="email" name="email" placeholder="Email"> <br>
+    <input type="password" name="password" placeholder="Password"> <br>
+    <input type="submit" name="register" placeholder="register"> <br>
+</form>
