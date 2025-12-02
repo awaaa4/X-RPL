3f6285b39d35137e0c4e24c94d5d92b0308a427f
Tsabitah Asilah Najwa <asilahnajwa04@gmail.com>
2025-12-02 13:07:38 +0700

    Add files via upload

---
+++ Kelas (PAK TRI)/18-SEPTEMBER-2025/projek/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Perulangan!" << endl<<endl;
+    cout<<"program Perulangan For"<<endl;
+
+
+
+        for (int i=1;j<=5;i++)
+            (
+                for (int j=1;j<=5;j++)
+                    (
+                        cout<<"ini i ke- "<<i<<"Dan ini J ke- "<<j;
+                        cout<<endl;
+                    )
+            )
+
+
+
+    return 0;
+}
